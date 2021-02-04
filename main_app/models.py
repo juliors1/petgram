9d9bf4c416a36333993e6c3bbe6551fdd9e08763
Juliors1 <juliorojassolis@gmail.com>
2021-02-04 16:33:17 -0500

    Create post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    caption = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+
