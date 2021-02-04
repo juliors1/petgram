2b9f821e3eda65bd07eb5017fbe5ba135d19a985
Juliors1 <juliorojassolis@gmail.com>
2021-02-04 16:00:03 -0500

    add route for create post

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("profile/",views.profile, name="profile")
+    path("post/create/",views.PostCreate.as_views(), name="post_create")
 
 
 ]
