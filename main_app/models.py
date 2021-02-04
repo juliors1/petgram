7032a675dab3013ea0159075e82f88eef4367756
Juliors1 <juliorojassolis@gmail.com>
2021-02-04 16:37:10 -0500

    import reverse() to redirect to home

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     caption = models.CharField(max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
+    def get_absolute_url(self):
+        return reverse("home", kwargs={"pk": self.id})
 
 
--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("post/create/",views.PostCreate.as_views(), name="post_create")
-
-
+   
 ]
