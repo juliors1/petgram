ea1d806924cad5277582105b44b1ba2e3118145c
Juliors1 <juliorojassolis@gmail.com>
2021-02-05 15:40:46 -0500

    import reverse to redirect me to my post details

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
     name = models.CharField(max_length=25)
-    caption = models.CharField(max_length=500)
-
+    caption = models.TextField(max_length=250)
 
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse("detail", kwargs={"post_id": self.id})
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"post_id": self.id})
