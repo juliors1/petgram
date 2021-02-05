68144dc76d762eaeca72c02ddbaf0faa3a53c5f5
Juliors1 <juliorojassolis@gmail.com>
2021-02-04 21:34:28 -0500

    create function to render post

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html")
 
+def posts_index(request):
+    return render(request, "posts/index.html", { "posts" : posts})
