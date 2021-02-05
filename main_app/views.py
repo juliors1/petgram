38af3b2122d71cd85fdc3a79b15aa16aa8f8dae5
Juliors1 <juliorojassolis@gmail.com>
2021-02-05 11:25:08 -0500

    import Post model to views and make index function

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 
 def home(request):
     return render(request, "home.html")
 
 def posts_index(request):
+    posts = Post.objects.all()
     return render(request, "posts/index.html", { "posts" : posts})
+
