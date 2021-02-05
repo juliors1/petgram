34e5b683c2c55902879a2d9f168ca7290eb1ce7a
Juliors1 <juliorojassolis@gmail.com>
2021-02-05 14:01:46 -0500

    write view function for posts_detail

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,6 @@ def posts_index(request):
     return render(request, "posts/index.html", {"posts": posts})
 
 
+def posts_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "posts/detail.html", {"post": post})
