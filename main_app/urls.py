7fee29fde6e4cd2fe1a9c4238c3e1b5d32af4fa5
Juliors1 <juliorojassolis@gmail.com>
2021-02-05 14:00:49 -0500

    create route for post details

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("posts/", views.posts_index, name="index"),
+    path("posts/<int:post_id>/", views.posts_detail, name="detail"),
+
+
 
 
 
