7bb1c8b91aab920bf3e5008d2e3341860e2e2d01
Juliors1 <juliorojassolis@gmail.com>
2021-02-06 17:34:43 -0500

    create route for PostDelete in urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("posts/<int:post_id>/", views.posts_detail, name="detail"),
     path("posts/create/", views.PostCreate.as_view(), name="posts_create"),
     path("posts/<int:pk>/update/", views.PostUpdate.as_view(), name="post_update"),
-
+    path("posts/<int:pk>/delete/", views.PostDelete.as_view(), name="post_delete"),
 
 
 
