3490640b21dadb905a3e44281bec333aa5cbccc5
Juliors1 <juliorojassolis@gmail.com>
2021-02-08 16:39:14 -0500

    add Profile Model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.CharField(max_length=200, blank=True)
+    bio = models.TextField(max_length=250, blank=True)
+
+
+    def __str__(self):
+        return f"Profile for user: {self.user.first_name} at user_id {self.user_id}, profile object {self.id}."
 
 class Post(models.Model):
     name = models.CharField(max_length=25)
@@ -9,6 +17,8 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name="post_likes")
 
+    def total_likes(self):
+        return self.likes.count()
 
     def __str__(self):
         return self.name
