04e2c7cb88c2ff1e739b089ceee8e19b83e3a3a0
Juliors1 <juliorojassolis@gmail.com>
2021-02-07 23:41:27 -0500

    create view function for signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth import login
@@ -33,3 +33,21 @@ def posts_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, "posts/detail.html", {"post": post})
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
