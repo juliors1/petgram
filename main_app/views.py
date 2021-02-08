84211515f7d7bb9fcab9c56d6471d30440cfa2ce
Juliors1 <juliorojassolis@gmail.com>
2021-02-08 00:34:44 -0500

    import boto3 and uuid

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Photo
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+import uuid
+import boto3
 
 
+S3_BASE_URL = "https://s3.us-east-1.amazonaws.com/"
+BUCKET = "petgram"
 class PostCreate(LoginRequiredMixin,CreateView):
     model = Post
     fields = ["name","caption"]
