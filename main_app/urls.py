6ba9db26bc9b2d3aa6ee0b040f341d8b0b96e73a
Juliors1 <juliorojassolis@gmail.com>
2021-02-08 00:30:45 -0500

    add the add_photo url

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path("posts/create/", views.PostCreate.as_view(), name="posts_create"),
     path("posts/<int:pk>/update/", views.PostUpdate.as_view(), name="post_update"),
     path("posts/<int:pk>/delete/", views.PostDelete.as_view(), name="post_delete"),
-    path('accounts/signup/', views.signup, name='signup'),
-
-
-
+    path("accounts/signup/", views.signup, name="signup"),
+    path("posts/<int:post_id>/add_photo/", views.add_photo, name="add_photo"),
 ]
