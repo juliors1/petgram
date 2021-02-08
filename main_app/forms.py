c8aa283eea107868dea96e48aedc688fc07d202b
Juliors1 <juliorojassolis@gmail.com>
2021-02-08 16:38:17 -0500

    create a UserForm

---
+++ main_app/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username')
