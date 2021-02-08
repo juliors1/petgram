bacb8f1e75f6281672649a2d5100af197b9f6dea
Juliors1 <juliorojassolis@gmail.com>
2021-02-08 00:59:44 -0500

    add route for adding a photo

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("posts/<int:pk>/update/", views.PostUpdate.as_view(), name="post_update"),
     path("posts/<int:pk>/delete/", views.PostDelete.as_view(), name="post_delete"),
     path("accounts/signup/", views.signup, name="signup"),
-    path("posts/<int:post_id>/add_photo/", views.add_photo, name="add_photo"),
+    path("posts/<int:post_id>/add_photo", views.add_photo, name="add_photo"),
 ]
