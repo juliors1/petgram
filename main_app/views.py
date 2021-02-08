ec6cc63c70ab467abdef50c6623c021788f0bcca
Juliors1 <juliorojassolis@gmail.com>
2021-02-08 00:37:40 -0500

    create add_photo view function

--- main_app/views.py
+++ main_app/views.py
@@ -59,3 +59,22 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+def add_photo(request, post_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to post_id or post (if you have a post object)
+            photo = Photo(url=url, post_id=post_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', post_id=post_id)
