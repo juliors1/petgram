0bf5485ec56f4f617f2f6fc7a0f449487aa15887
Juliors1 <juliorojassolis@gmail.com>
2021-02-08 16:44:15 -0500

    Config deployment

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 from .models import Post, Photo, User
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .forms import UserForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
