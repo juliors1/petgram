60d345a393640c347a0f621e5a6f7236d53d025a
Juliors1 <juliorojassolis@gmail.com>
2021-02-10 04:46:03 -0500

    add a __str__ function in photo model

--- main_app/models.py
+++ main_app/models.py
@@ -3,13 +3,10 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    photo = models.CharField(max_length=200, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.TextField(default='https://mspgh.unimelb.edu.au/__data/assets/image/0011/3576098/Placeholder.jpg')
     bio = models.TextField(max_length=250, blank=True)
 
-
-    def __str__(self):
-        return f"Profile for user: {self.user.first_name} at user_id {self.user_id}, profile object {self.id}."
 
 class Post(models.Model):
     name = models.CharField(max_length=25)
@@ -29,3 +26,14 @@ class Post(models.Model):
 class Photo(models.Model):
     url = models.CharField(max_length=200)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for post_id: {self.post_id} @{self.url}"
+
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+#     body = models.TextField()
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return "%s - %s" % (self.name)
